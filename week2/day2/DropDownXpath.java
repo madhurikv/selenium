8257ff3b582edc6cf39bb2f273968a0f6cf2dc87
madhurikv <85861174+madhurikv@users.noreply.github.com>
2021-06-27 10:11:31 +0400

    Add files via upload
    updated the last selection

--- week2/day2/DropDownXpath.java
+++ week2/day2/DropDownXpath.java
@@ -28,37 +28,46 @@ public class DropDownXpath {
 			
 			WebElement webElement1 = driver.findElement(By.id("dropdown1"));
 			Select drpDown1 = new Select(webElement1);
-			drpDown1.selectByVisibleText("Selenium");
+			drpDown1.selectByIndex(2);;
 			
 			WebElement webElement2 = driver.findElement(By.name("dropdown2"));
 			Select drpDown2 = new Select(webElement2);
-			drpDown2.selectByValue("3");
+			drpDown2.selectByVisibleText("Selenium");
 			
 			WebElement webElement3 = driver.findElement(By.id("dropdown3"));
 			Select drpDown3 = new Select(webElement3);
-			drpDown3.selectByVisibleText("Loadrunner");
+			drpDown3.selectByValue("2");
 			
 			WebElement webElement4 = driver.findElement(By.className("dropdown"));
 			Select drpDown4 = new Select(webElement4);
+			//size of the list
+			int size = drpDown4.getOptions().size();
+			System.out.println("size of the drop down list " +size);
 			
 			
 			drpDown4.selectByVisibleText("Loadrunner");
 			
-			WebElement webElement5 = driver.findElement(By.xpath("//select[@class='dropdown']/following::select"));
-			Select drpDown5 = new Select(webElement5);
-			drpDown5.selectByVisibleText("UFT/QTP");
+			driver.findElement(By.xpath("//select[@class='dropdown']/following::select")).sendKeys("UFT/QTP");
+			//Select drpDown5 = new Select(webElement5);
+			//drpDown5.selectByVisibleText("UFT/QTP");
+
+WebElement webElement5 = driver.findElement(By.xpath("//div[@id='contentblock']//following::select[5]"));
+Select drpDown5 = new Select(webElement5);
+drpDown5.selectByVisibleText("UFT/QTP");
+
 			
-			WebElement webElement6 = driver.findElement(By.xpath("//select[@class='dropdown']/following::select[2]"));
+			/*WebElement webElement6 = driver.findElement(By.xpath("//select[@class='dropdown']/following::select[2]"));
 			Select drpDown6 = new Select(webElement6);
 			if(drpDown6.isMultiple()){
 				drpDown6.selectByIndex(2);
 				drpDown6.selectByIndex(3);
-			}
-		
+			}*/
+			driver.findElement(By.xpath("//option[text()='Select your programs']/following-sibling::option")).click();
+			driver.findElementByXPath("(//option[text()='Select your programs']/following-sibling::option)[2]").click();
 			
 			driver.close();
 			
-
+			
 		}
 
 	}
